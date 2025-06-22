7e1c49135e21734716b8fc7cc861aa7ab86be0d0
mmadc0der <tim110702@mail.ru>
2025-06-22 17:07:09 +0300

    fix: imports

--- bindings/py_module.cpp
+++ bindings/py_module.cpp
@@ -10,7 +10,7 @@ PYBIND11_MODULE(rlchess_engine, m) {
     py::class_<Board>(m, "Board")
         .def(py::init<>())
         .def(py::init<const std::string &>(), py::arg("fen"))
-        .def("fen", &Board::fen, "Возвращает FEN представление позиции")
+        .def("fen", &Board::fen, "Returns FEN representation of the position")
         .def("to_string", &Board::to_string)
         .def("__str__", &Board::to_string);
 }
